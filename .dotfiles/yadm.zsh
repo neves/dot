cb5b709bfc3a7adbdcc46af0b5bc48181f439df4
Marcos Neves <marcos.neves@gmail.com>
2018-02-18 10:52:47 -0300

    M .dotfiles/yadm.zsh M .gitconfig A .gitconfig-user

--- .dotfiles/yadm.zsh
+++ .dotfiles/yadm.zsh
@@ -10,7 +10,7 @@ alias ydc="yadm icdiff --cached"
 # Listar todos os arquivos adicionados no git
 alias yl="yadm ls-files"
 # commit com os arquivos modificados como mensagem
-alias yadm_status_for_commit_message='yadm status -s | tr -s "\n " "  " | awk ''{$1=$1};1'''
+alias yadm_status_for_commit_message='yadm diff --cached --name-status | tr -s "\n " "  " | awk ''{$1=$1};1'''
 
 yc() {
 	if [[ $# -gt 0 ]]; then
