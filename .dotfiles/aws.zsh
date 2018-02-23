6f965514edfe33bbcac8f04a26214d2d5f80e7ca
Marcos Neves <marcos.neves@gmail.com>
2018-02-23 10:02:44 -0300

    Lazy load aws completion

--- .dotfiles/aws.zsh
+++ .dotfiles/aws.zsh
@@ -1,10 +1,14 @@
-if which aws > /dev/null; then
-  ASDF_PYTHON=$(asdf which python 2> /dev/null)
-  #  python foi instalado utilizando asdf
-  if [ "$ASDF_PYTHON" ]; then
-    source "$(dirname $ASDF_PYTHON)/aws_zsh_completer.sh"
-  else
-    # Não consegue carregar se (which aws_zsh_completer.sh) for um shim
-    source "${ZDOTDIR:-$HOME}/.oh-my-zsh/plugins/aws/aws.plugin.zsh"
+_aws() {
+  unset -f _aws
+  if which aws > /dev/null; then
+    ASDF_PYTHON=$(asdf which python 2> /dev/null)
+    #  python foi instalado utilizando asdf
+    if [ "$ASDF_PYTHON" ]; then
+      source "$(dirname $ASDF_PYTHON)/aws_zsh_completer.sh"
+    else
+      # Não consegue carregar se (which aws_zsh_completer.sh) for um shim
+      source "${ZDOTDIR:-$HOME}/.oh-my-zsh/plugins/aws/aws.plugin.zsh"
+    fi
   fi
-fi
+  _aws
+}
