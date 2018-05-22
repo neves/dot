8a1cc99ffec170cfc14dd6030f4ea20f9e7f33bd
Marcos Neves | air <marcos.neves@gmail.com>
2018-05-22 10:40:23 -0300

    M .dotfiles/setup/ubuntu/apt.sh

--- .dotfiles/setup/ubuntu/apt.sh
+++ .dotfiles/setup/ubuntu/apt.sh
@@ -1,7 +1,9 @@
 sudo apt update
-sudo apt install -y sqlite3 autoconf bison build-essential make patch ruby-dev pkg-config \
-libssl-dev libyaml-dev libbz2-dev zlib1g-dev libffi-dev libgdbm3 libgdbm-dev libxml2 libxml2-dev libxslt1-dev liblzma-dev \
-libsqlite3-dev libmysqlclient-dev libncurses5-dev libncursesw5-dev libreadline-dev libreadline6-dev libpq-dev \
-llvm tk-dev xz-utils apt-transport-https ca-certificates software-properties-common \
-zsh curl git gitk icdiff xclip silversearcher-ag python-pip direnv \
-chrome-gnome-shell gnome-tweak-tool gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 git-extras
+sudo apt install -y sqlite3 autoconf bison build-essential make patch llvm \
+ruby-dev pkg-config libgdbm3 libgdbm-dev libxml2 libxml2-dev libxslt1-dev \
+libssl-dev libyaml-dev libbz2-dev zlib1g-dev libffi-dev liblzma-dev \
+libsqlite3-dev libmysqlclient-dev libpq-dev \
+libncurses5-dev libncursesw5-dev libreadline-dev libreadline6-dev \
+xz-utils apt-transport-https ca-certificates software-properties-common tk-dev \
+zsh curl git git-extras gitk icdiff xclip silversearcher-ag python-pip direnv \
+chrome-gnome-shell gnome-tweak-tool gir1.2-gtop-2.0 gir1.2-networkmanager-1.0
