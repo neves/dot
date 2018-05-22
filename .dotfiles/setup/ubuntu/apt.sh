3409224e00f48b801ac8d4ef6dba1d8d08def60d
Marcos Neves | air <marcos.neves@gmail.com>
2018-05-22 10:47:43 -0300

    M .dotfiles/setup/ubuntu/apt.sh

--- .dotfiles/setup/ubuntu/apt.sh
+++ .dotfiles/setup/ubuntu/apt.sh
@@ -1,9 +1,9 @@
 sudo apt update
 sudo apt install -y sqlite3 autoconf bison build-essential make patch llvm \
-ruby-dev pkg-config libgdbm3 libgdbm-dev libxml2 libxml2-dev libxslt1-dev \
-libssl-dev libyaml-dev libbz2-dev zlib1g-dev libffi-dev liblzma-dev \
+ruby-dev pkg-config libgdbm5 libgdbm-dev libxml2 libxml2-dev libxslt1-dev \
+libssl1.0-dev libyaml-dev libbz2-dev zlib1g-dev libffi-dev liblzma-dev \
 libsqlite3-dev libmysqlclient-dev libpq-dev \
-libncurses5-dev libncursesw5-dev libreadline-dev libreadline6-dev \
+libncurses5-dev libncursesw5-dev libreadline6-dev \
 xz-utils apt-transport-https ca-certificates software-properties-common tk-dev \
 zsh curl git git-extras gitk icdiff xclip silversearcher-ag python-pip direnv \
 chrome-gnome-shell gnome-tweak-tool gir1.2-gtop-2.0 gir1.2-networkmanager-1.0
