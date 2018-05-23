e5ca0da3e668c5c8d5be73415c8dce6f74a50656
Marcos Neves | air <marcos.neves@gmail.com>
2018-05-23 17:16:52 -0300

    M .dotfiles/setup/ubuntu/apt.sh

--- .dotfiles/setup/ubuntu/apt.sh
+++ .dotfiles/setup/ubuntu/apt.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install -y sqlite3 autoconf bison build-essential make patch llvm \
 ruby-dev pkg-config libgdbm5 libgdbm-dev libxml2 libxml2-dev libxslt1-dev \
 libssl1.0-dev libyaml-dev libbz2-dev zlib1g-dev libffi-dev liblzma-dev \
-libsqlite3-dev libmysqlclient-dev libpq-dev \
+libsqlite3-dev libmysqlclient-dev libpq-dev yadm \
 libncurses5-dev libncursesw5-dev libreadline6-dev \
 xz-utils apt-transport-https ca-certificates software-properties-common tk-dev \
 zsh curl git git-extras gitk icdiff xclip silversearcher-ag python-pip direnv \
